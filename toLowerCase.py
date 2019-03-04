266bf052e72526f75da3e69e532abee11bb5acd4
Casey <47370829+casey-rjones@users.noreply.github.com>
2019-03-04 12:31:19 -0600

    Update toLowerCase.py

--- toLowerCase.py
+++ toLowerCase.py
@@ -1,7 +1,22 @@
-#
-# Implement function ToLowerCase() that has a string parameter str, 
-# and returns the same string in lowercase.
-#
+ #
+ # toLowerCase.py
+ #
+ # Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+ #
+ # Example 1:
+ #
+ # Input: "Hello"
+ # Output: "hello"
+ #
+ # Example 2:
+ #
+ # Input: "PiTAs"
+ # Output: "pitas"
+ # Example 3:
+ #
+ # Input: "love"
+ # Output: "love"
+ #
 
 class Solution(object):
     def toLowerCase(self, str):
