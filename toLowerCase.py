f2ac226073cd89cb70c2c6750a464187ba26998c
Casey <47370829+casey-rjones@users.noreply.github.com>
2019-03-04 12:25:03 -0600

    Create toLowerCase.py

---
+++ toLowerCase.py
@@ -0,0 +1,38 @@
+#
+# Implement function ToLowerCase() that has a string parameter str, 
+# and returns the same string in lowercase.
+#
+
+class Solution(object):
+    def toLowerCase(self, str):
+        outputStr=''
+        for s in str:
+            if 'A' <= s and s <= 'Z':
+                outputStr += s.lower()
+            else:
+                outputStr += s   
+        return outputStr
+
+def stringToString(input):
+    return input[1:-1].decode('string_escape')
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+    lines = readlines()
+    while True:
+        try:
+            line = lines.next()
+            str = stringToString(line)
+            
+            ret = Solution().toLowerCase(str)
+
+            out = (ret)
+            print out
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
